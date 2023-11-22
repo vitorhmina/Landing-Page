afa5d2aa7f9c41a61a5a25222c704da0ada2fb84
rekqN <pmreisf@gmail.com>
2023-11-22 13:13:24 +0000

    Small adjustments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 
 AOS.init({
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
   offset: 120, // offset (in px) from the original trigger point
   delay: 0, // values from 0 to 3000, with step 50ms
   duration: 900, // values from 0 to 3000, with step 50ms
